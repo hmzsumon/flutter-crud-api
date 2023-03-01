462815fd57e5a4b23504ab4d1f7a6bb44f5a555a
Zakaria <zaknetsum@gmail.com>
2023-03-01 23:18:31 +0600

    eroorHnadle edit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,5 @@
 const Product = require('../models/Product');
-const ErrorHandler = require('../utils/errorHandler');
+const ErrorHandler = require('../utils/errorhandler');
 const catchAsyncErrors = require('../middleware/catchAsyncErrors');
 const cloudinary = require('cloudinary');
 
